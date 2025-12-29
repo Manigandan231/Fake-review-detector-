e86f7bf24a7c7bc7144f0d2ed4644fdacad2e885
Manigandan231 <manikandansaravanan121@gmail.com>
2025-12-29 12:22:47 +0530

    Implemented data preprocessing using NLP and TF-IDF

--- train.py
+++ train.py
@@ -1,1 +1,37 @@
+import pandas as pd
+import nltk
+import re
+
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# Download stopwords (first time only)
+nltk.download('stopwords')
+
+# Load dataset
+data = pd.read_csv(r"C:\Users\Manikandansaravanan\OneDrive\Documents\Fake-review-detector--main\data\fake_reviews_dataset.csv")
+
+# Select columns
+texts = data['text']
+labels = data['label']
+
+# Text cleaning function
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'[^a-z\s]', '', text)
+    return text
+
+# Apply cleaning
+texts = texts.apply(clean_text)
+
+# TF-IDF Vectorization
+vectorizer = TfidfVectorizer(
+    stop_words=stopwords.words('english'),
+    max_features=5000
+)
+
+X = vectorizer.fit_transform(texts)
+
+print("Data preprocessing completed")
+print("Feature shape:", X.shape)
 
